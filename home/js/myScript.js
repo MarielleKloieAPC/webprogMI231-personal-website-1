f999b303a7adcb72bd891bd8906c1a4f530db941
Marielle Kloie D. Concepcion <mdconcepcion@student.apc.edu.ph>
2024-12-16 20:04:59 +0800

    Added another js function

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -1,3 +1,8 @@
 function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
-  }
+  document.getElementById("demo").innerHTML = "I like the flavor Matcha!";
+}
+function myFunction2() {
+  const element = document.getElementsByTagName("p");
+  
+  document.getElementById("demo").innerHTML = "Extra";
+}
