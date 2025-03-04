b654b1e725a9def8f863f52c4b16491299f12c3d
Marielle Kloie D. Concepcion <mdconcepcion@student.apc.edu.ph>
2025-03-04 21:59:26 +0000

    Update

---
+++ my-website-finals/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Hello</h1>
+  <country/><br>
+  <food-item/> <br>
+  <food-item2/> <br>
+  <food-item/> <br>
+  <comment-form/>
+  <comment/><br>
+  <personal-profile/> <br>
+   
+
+</template>
+
+<script></script>
